7d6ea0bff24c90908e4503c426f3a33edfd3f716
Dylan <dylan4047@gmail.com>
2017-02-03 15:32:23 +0100

    rajout des condition pour instancier les bonne function du controller

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
  <?php 
-require 'Controllers/ArmoireController.php';
+ require 'Controllers/ArmoireController.php';
 
-if (!isset($_GET['id'])) {
-	(new ArmoireController())->home();
-}else{
-	(new ArmoireController())->detail();
+ if (!isset($_GET['id'])) {
+ 	(new ArmoireController())->home();
+ }
+ if (isset($_GET['id']) && !isset($_GET['action'])){
+ 	(new ArmoireController())->detail();
+ }
+else if ($_GET['action'] === 'edit' && isset($_GET['id'])) {
+	(new ArmoireController())->edit();
 }
 
-  ?>
+
+
+ ?>
